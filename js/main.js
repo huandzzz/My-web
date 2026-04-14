a696a29a2b3bd16f0eb584a30e67026246b21299
huandzzz <huanminh960@gmail.com>
2026-04-14 18:38:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function loadProducts() {
     products.forEach(p => {
         html += `
             <div class="product">
-                <img src="${p.img}" style="width:100%; border-radius:10px;">
+                <img src="${images.webp}" style="width:100%; border-radius:10px;">
                 <h3>${p.name}</h3>
                 <p>${p.price.toLocaleString()} VNĐ</p>
             </div>
