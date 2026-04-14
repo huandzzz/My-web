3c95f5be9053ec07964df19bbceeef7d7e7a0c8b
huandzzz <huanminh960@gmail.com>
2026-04-14 18:35:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 function loadProducts() {
     let html = "";
+
     products.forEach(p => {
-        html += `<p>${p.name} - ${p.price} VNĐ</p>`;
+        html += `
+            <div class="product">
+                <img src="${p.img}" style="width:100%; border-radius:10px;">
+                <h3>${p.name}</h3>
+                <p>${p.price.toLocaleString()} VNĐ</p>
+            </div>
+        `;
     });
+
     document.getElementById("productList").innerHTML = html;
 }
