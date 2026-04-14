5ab51d0745217744f44032fff0e817fe809b506c
huandzzz <huanminh960@gmail.com>
2026-04-14 18:17:30 +0700

    Delete css/js directory

--- css/js/data.js
+++
@@ -1,5 +0,0 @@
-const products = [
-    { name: "Laptop", price: 15000 },
-    { name: "Chuột", price: 500 },
-    { name: "Bàn phím", price: 1000 }
-];
--- css/js/main.js
+++
@@ -1,7 +0,0 @@
-function loadProducts() {
-    let html = "";
-    products.forEach(p => {
-        html += `<p>${p.name} - ${p.price} VNĐ</p>`;
-    });
-    document.getElementById("productList").innerHTML = html;
-}
